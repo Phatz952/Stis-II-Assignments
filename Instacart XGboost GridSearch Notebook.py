3d32ef9a7fb1b1ecaab3ee53d167922ae8c8bcf4
Panagiotis Hatziioannou <63719321+Phatz952@users.noreply.github.com>
2020-05-31 23:31:04 +0300

    Update Instacart XGboost GridSearch Notebook.py

--- Instacart XGboost GridSearch Notebook.py
+++ Instacart XGboost GridSearch Notebook.py
@@ -83,7 +83,7 @@ gc.enable()                       # Activate
 
 # connect to kaggle api and download files (zip)
 from kaggle.api.kaggle_api_extended import KaggleApi
-api = KaggleApi({"username":"phatz952","key":"bae60ef5634eb395296b95934eda024d"})
+api = KaggleApi({"username":"phatz952","key":"3f242319262be0dc1e68af789211a144"})
 api.authenticate()
 files = api.competition_download_files("Instacart-Market-Basket-Analysis")
 
@@ -951,7 +951,7 @@ model = xgbc.fit(X_train, y_train)
 ##################################
 # FEATURE IMPORTANCE - GRAPHICAL
 ##################################
-xgb.plot_importance(model)
+#xgb.plot_importance(model)
 
 
 # ## 4.2 Fine-tune your model
